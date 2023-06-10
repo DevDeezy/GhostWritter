e76b31b062cadeb46d1f659e9bfd27bfa98ae58c
BrunoDBliss <bdionisio@blissapplications.com>
2023-06-10 18:33:59 +0100

    Add global layout and switch between Scenes

--- GhostWritterProject/Assets/MainMenuController.cs
+++ GhostWritterProject/Assets/MainMenuController.cs
@@ -7,21 +7,46 @@ public class MainMenuController : MonoBehaviour
 {
     public CanvasGroup OptionPanel;
 
-    public void PlayGame()
+    public void PlayTutorial()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SelectLevel()
+    {
+        OptionPanel.alpha = 1;
+        OptionPanel.blocksRaycasts = true;
+    }
+
+    public void LevelEasy()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LevelMedium()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LevelHard()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Rules()
     {
-        OptionPanel.alpha = 1;
-        OptionPanel.blocksRaycasts = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void Back()
     {
         OptionPanel.alpha = 0;
         OptionPanel.blocksRaycasts = false;
+    }
+
+    public void BackScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
     }
 
     public void QuitGame()
